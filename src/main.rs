490de2986cc1ed0408339da5d773a3bc90c94c64
Anish Sharma <aneeshsharma15@outlook.com>
2021-12-02 17:05:25 +0530

    fixed username

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ fn main() {
     let mut username = String::new();
     stdin().read_line(&mut username).unwrap();
 
+    username = String::from(username.trim());
+
     let tracker_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(149, 129, 129, 226)), 8982);
     let aether = Aether::new(username, tracker_addr);
 
     println!("Enter peer username: ");
     let mut peer_username = String::new();
     stdin().read_line(&mut peer_username).unwrap();
+
+    peer_username = String::from(peer_username.trim());
 
     aether.start();
 
