55940864fec7072ab2c2e1d099ec3a9e99c96f50
Anish Sharma <aneeshsharma15@outlook.com>
2021-12-02 17:03:09 +0530

    added simple connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use aether_lib::peer::Aether;
+use std::io::stdin;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter username: ");
+    let mut username = String::new();
+    stdin().read_line(&mut username).unwrap();
+
+    let tracker_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(149, 129, 129, 226)), 8982);
+    let aether = Aether::new(username, tracker_addr);
+
+    println!("Enter peer username: ");
+    let mut peer_username = String::new();
+    stdin().read_line(&mut peer_username).unwrap();
+
+    aether.start();
+
+    aether.connect(peer_username.clone());
+
+    loop {}
 }
